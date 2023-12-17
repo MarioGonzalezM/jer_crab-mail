231fb2d43121a5c4707c1b0c510eb228a19bf8c5
MarioGonzalezM <m.gonzalezm.2021@alumnos.urjc.es>
2023-12-17 16:36:53 +0100

    Añadida pagina de login
    Se ha añadido una primera version de la pagina de login al juego, actualmente se puede acceder a ella pulsando el boton de controles o online en el menu

--- project1/src/main/resources/static/Scenes/UnderConstructionScene.js
+++ project1/src/main/resources/static/Scenes/UnderConstructionScene.js
@@ -7,15 +7,44 @@ class UnderConstructionScene extends Phaser.Scene {
     range = 5;
     preload()
     {
+		
+		/*
         this.load.image('backgroundU', 'Assets/UnderConstructionMenu/Background.png')
         this.load.image('chains', 'Assets/UnderConstructionMenu/Chains.png')
         this.load.image('foreground', 'Assets/UnderConstructionMenu/Foreground.png')
         this.load.image('return', 'Assets/UnderConstructionMenu/ReturnButton.png')
         this.load.image('shadow', 'Assets/UnderConstructionMenu/Shadow.png')
+        */
     }
     create() {
+		 var iframe = document.createElement('iframe');
+
+    // Establecer la URL del documento HTML que quieres cargar
+    iframe.src = 'Scenes/index1.html';
+
+    // Establecer dimensiones y posición del iframe
+    iframe.width = 600;
+    iframe.height = 400;
+    iframe.style.position = 'absolute';
+    iframe.style.top = '50px';
+    iframe.style.left = '100px';
+
+    // Agregar el iframe al DOM
+    document.body.appendChild(iframe);
+    var self = this;
+    // Escuchar mensajes del iframe
+    window.addEventListener('message', function (event) {
+        if (event.data === 'cambiarEscena') {
+            // Lógica para cambiar la escena en Phaser
+            self.scene.sleep('UnderConstructionScene');
+        	self.scene.start('MainMenu');
+            console.log('Cambiando a la nueva escena de Phaser');
+            iframe.parentNode.removeChild(iframe);
+            // Puedes agregar aquí la lógica específica para cambiar la escena de Phaser
+        }
+    });
         //Add all images
-        this.add.image(960, 540, 'backgroundU');
+        /*this.add.image(960, 540, 'backgroundU');
         var shadow = this.add.image(946, 470, 'shadow').setOrigin(0.5,-0.75);
         var chains = this.add.image(960, 540, 'chains');
         var button = this.add.image(946, 575, 'return').setOrigin(0.5,-1);
@@ -27,7 +56,7 @@ class UnderConstructionScene extends Phaser.Scene {
             scene.scene.start('MainMenu');
         });
 
-        /**/button.on('pointerover',function ()
+        button.on('pointerover',function ()
         {
             scene.buttonTween = scene.tweens.add({
                 targets: [button, chains,shadow],
@@ -61,6 +90,6 @@ class UnderConstructionScene extends Phaser.Scene {
                 yoyo: false,
                 repeat: 0
             });
-        })//*/
+        })*/
     }
 }
